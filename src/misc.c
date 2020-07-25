28c927d1e08f3e3248ef69eff24007b34f4a5728
kattaka <ktu.embddev@gmail.com>
2020-07-25 15:43:13 +0900

    append including header.

--- src/misc.c
+++ src/misc.c
@@ -16,6 +16,8 @@
 ** limitations under the License.
 */
 
+#include "misc.h"
+
 /** returns 1 if line starts with prefix, 0 if it does not */
 int strStartsWith(const char *line, const char *prefix)
 {
