7ec4660e97dc5bed645b0316a64f2f72c8f3115f
kattaka <ktu.embddev@gmail.com>
2020-07-26 17:07:55 +0900

    append setting bitrate.

--- src/atchannel.c
+++ src/atchannel.c
@@ -575,8 +575,57 @@ static void clearPendingCommand(ATChannel* atch)
 
 ATReturn  at_open(ATChannel* atch)
 {
-    int fd = 0;
+    struct bitrate_value {
+        int bitrate;
+        speed_t value;
+    };
 
+    static const struct bitrate_value bitrateValues[] = {
+        {      0,       B0},
+        {     50,      B50},
+        {     75,      B75},
+        {    110,     B110},
+        {    134,     B134},
+        {    150,     B150},
+        {    200,     B200},
+        {    300,     B300},
+        {    600,     B600},
+        {   1200,    B1200},
+        {   1800,    B1800},
+        {   2400,    B2400},
+        {   4800,    B4800},
+        {   9600,    B9600},
+        {  19200,   B19200},
+        {  38400,   B38400},
+        {  57600,   B57600},
+        { 115200,  B115200},
+        { 230400,  B230400},
+        { 460800,  B460800},
+        { 500000,  B500000},
+        { 576000,  B576000},
+        { 921600,  B921600},
+        {1000000, B1000000},
+        {1152000, B1152000},
+        {1500000, B1500000},
+        {2000000, B2000000},
+        {2500000, B2500000},
+        {3000000, B3000000},
+        {3500000, B3500000},
+        {4000000, B4000000},
+    };
+
+    speed_t speed = (speed_t)-1;
+    for (unsigned int i = 0; i < NUM_ELEMS(bitrateValues); i++) {
+        if (atch->bitrate == bitrateValues[i].bitrate) {
+            speed = bitrateValues[i].value;
+        }
+    }
+    if (speed == (speed_t)-1) {
+        RLOGE(atch, "specified bitrate %d is invalid value.", atch->bitrate);
+        return AT_ERROR_GENERIC;
+    }
+
+    int fd = 0;
     fd = open(atch->path, O_RDWR);
     if (fd < 0) {
         RLOGE(atch, "opening port %s failed.", atch->path);
@@ -586,6 +635,8 @@ ATReturn  at_open(ATChannel* atch)
 
     struct termios ios;
     tcgetattr(fd, &ios);
+    cfsetispeed(&ios, speed);
+    cfsetospeed(&ios, speed);
     ios.c_lflag = atch->lflag;
     tcsetattr(fd, TCSANOW, &ios);
 
